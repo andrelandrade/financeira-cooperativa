5c51aeba46f1856fd0e16c2120485136dc9c9c27
André Luis de Andrade <al.andrade256@gmail.com>
2023-09-08 10:27:08 -0300

    Commit inicial de associado

---
+++ associado/src/test/java/com/fincoop/associado/AssociadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincoop.associado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
