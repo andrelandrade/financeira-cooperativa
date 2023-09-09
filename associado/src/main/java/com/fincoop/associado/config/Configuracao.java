2109e1c9b99ff1bfa2b37deccabacdb776d3f45d
André Luis de Andrade <al.andrade256@gmail.com>
2023-09-09 15:49:24 -0300

    Alterando configuração do modelmapper pra não mapear se estíver null

--- associado/src/main/java/com/fincoop/associado/config/Configuracao.java
+++ associado/src/main/java/com/fincoop/associado/config/Configuracao.java
@@ -1,5 +1,6 @@
 package com.fincoop.associado.config;
 
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,10 @@ import org.springframework.context.annotation.Configuration;
 public class Configuracao {
     @Bean
     public ModelMapper getModelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+        return modelMapper;
     }
 }
