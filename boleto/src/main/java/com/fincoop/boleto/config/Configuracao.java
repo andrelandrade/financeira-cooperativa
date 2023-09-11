3190e2f181aae3011541235e82e147637a9f9c1a
André Luis de Andrade <al.andrade256@gmail.com>
2023-09-11 18:30:54 -0300

    Adicionando modelmapper ao projeto

---
+++ boleto/src/main/java/com/fincoop/boleto/config/Configuracao.java
@@ -0,0 +1,19 @@
+package com.fincoop.boleto.config;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        var modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+        return modelMapper;
+    }
+}
